0280483423c0517af8b7e486fd7adbdcdf74a2a3
Rodrigo de Souza Machado <107262755+Cwbmachado@users.noreply.github.com>
2023-11-26 10:49:04 -0300

    Tela Login(Front-end)

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using AcessiDOC1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class LoginController : Controller
+{
+   
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Autenticar(Usuario usuario)
+    {
+        // Lógica de autenticação 
+        if (usuario.NomeUsuario == "usuario" && usuario.Senha == "senha")
+        {
+            // Autenticação bem-sucedida, redirecionar para a página principal
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            // Autenticação falhou, exibir mensagem de erro
+            ViewBag.MensagemErro = "Nome de usuário ou senha incorretos.";
+            return View("Index");
+        }
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AcessiDOC1.Models
+{
+    public class Usuario
+    {
+        public string? NomeUsuario { get; set; }
+        public string? Senha { get; set; }
+    }
+
+}
